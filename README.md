64c67fb0a83ef23bf72a7c0b961ac9ff21abcada
tocchimittsu <arimagedon21@gmail.com>
2021-05-13 17:15:54 +0900

    修正

---
+++ README.md
@@ -0,0 +1,5 @@
+# tkinter-button
+Tkinterのbuttonについてのコード
+
+# 参考記事
+https://kuroro.blog/python/oFju6EngDtcYtIiMIDf1/
---
+++ main.py
@@ -0,0 +1,131 @@
+import tkinter as tk
+
+
+class Application(tk.Frame):
+    # 氏名入力用変数
+    entryName = None
+    # パスワード入力用変数
+    entryPassword = None
+
+    # クリアボタンがクリックされた場合に実行される関数
+    # 氏名、パスワードの入力データをクリアする。
+    def clearCommand(self):
+        # delete : 氏名情報の入力削除を行う。
+        # 第一引数 : 削除する文字列の開始位置(0番目からスタート)
+        # 第二引数 : 削除する文字列の最終位置
+        # tk.END : 文字列の最終位置
+        self.entryName.delete(0, tk.END)
+
+        # delete : パスワード情報の入力削除を行う。
+        # 第一引数 : 削除する文字列の開始位置(0番目からスタート)
+        # 第二引数 : 削除する文字列の最終位置
+        # tk.END : 文字列の最終位置
+        self.entryPassword.delete(0, tk.END)
+
+        print('名前とパスワードをクリアしました。')
+
+    # 追加ボタンがクリックされた場合に、氏名入力の末尾に"Bob"を入力追加する関数
+    def addNameBobButton(self):
+        # insert : 文字列の挿入を行う。
+        # 第一引数 : 追加文字列の挿入開始位置(0番目からスタート)
+        # tk.END : 文字列の最終位置
+        # 第二引数 : 追加する文字列情報
+        self.entryName.insert(tk.END, "Bob")
+
+    # パスワード確認ボタンがクリックされた場合に実行される関数
+    def getPassword(self):
+        # get : 現在入力されている情報を表示する関数
+        print('パスワード : ' + self.entryPassword.get())
+
+    def __init__(self, master=None):
+        # Windowの初期設定を行う。
+        super().__init__(master)
+
+        # Windowの画面サイズを設定する。
+        self.master.geometry("300x200")
+
+        ############################################## frame Widget START ##############################################
+
+        # Windowを親要素として、frame Widget(Frame)を作成する。
+        # frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+        frame = tk.Frame(self.master)
+
+        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        frame.pack()
+
+        ############################################## frame Widget END ##############################################
+
+        ############################################## entry Widget START ##############################################
+
+        # frame Widget(Frame)を親要素として、entry Widgetを作成する。
+        # text : 文字入力に紐づくラベル。名前のようなもの。複数entry Widgetを用意して、同じtextの名前を設定すると、同時に同名のentry Widgetが編集される。
+        # width : 幅の設定
+        self.entryName = tk.Entry(frame, text="entryName", width=15)
+
+        # frame Widget(Frame)を親要素とした場合に、entry Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        self.entryName.pack()
+
+        # 氏名の入力先へデフォルトで格納したい文字列の挿入。
+        # 第一引数 : 文字列の挿入開始位置(0番目からスタート)
+        # 第二引数 : 挿入文字列
+        self.entryName.insert(0, "氏名")
+
+        # frame Widget(Frame)を親要素として、entry Widgetを作成する。
+        # text : 文字入力に紐づくラベル。名前のようなもの。複数entry Widgetを用意して、同じtextの名前を設定すると、同時に同名のentry Widgetが編集される。
+        # width : 幅の設定
+        self.entryPassword = tk.Entry(frame, text="entryPassword", width=15)
+
+        # パスワードの入力先へデフォルトで格納したい文字列の挿入。
+        # 第一引数 : 文字列の挿入開始位置(0番目からスタート)
+        # 第二引数 : 挿入文字列
+        self.entryPassword.insert(0, "パスワード")
+
+        # frame Widget(Frame)を親要素とした場合に、entry Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        self.entryPassword.pack()
+
+        ############################################## entry Widget END ##############################################
+
+        ############################################## button Widget START ##############################################
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # text : テキスト情報
+        # width : ボタンの幅設定
+        # command : ボタンをクリックした場合に、実行する関数を設定する。
+        # buttonについて : https://kuroro.blog/python/oFju6EngDtcYtIiMIDf1/
+        confirmPasswordButton = tk.Button(frame, text="パスワード確認", width=15, command=self.getPassword)
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        confirmPasswordButton.pack(side=tk.LEFT)
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # text : テキスト情報
+        # command : ボタンをクリックした場合に、実行する関数を設定する。
+        # buttonについて : https://kuroro.blog/python/oFju6EngDtcYtIiMIDf1/
+        addNameBobButton = tk.Button(frame, text="追加", command=self.addNameBobButton)
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        addNameBobButton.pack(side=tk.LEFT)
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # text : テキスト情報
+        # command : ボタンをクリックした場合に、実行する関数を設定する。
+        # buttonについて : https://kuroro.blog/python/oFju6EngDtcYtIiMIDf1/
+        clearButton = tk.Button(frame, text="クリア", command=self.clearCommand)
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        clearButton.pack(side=tk.LEFT)
+
+        ############################################## button Widget END ##############################################
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    # Windowをループさせて、継続的にWindow表示させる。
+    app.mainloop()
